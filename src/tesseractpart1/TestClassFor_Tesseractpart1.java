354fdda019b2417ae59551d7b7508af4d801c8af
samarthm <samarthmadduru@sillygoose.T-mobile.com>
2016-04-22 17:24:08 -0500

    Upload
    This is the first iteration with the base classes, methods, and
    constructors. This is the basis for the OCR.

---
+++ src/tesseractpart1/TestClassFor_Tesseractpart1.java
@@ -0,0 +1,23 @@
+package tesseractpart1;
+
+
+import tesseractpart1.Tesseractpart1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+ 
+
+/**
+ *
+ * @author samarthmadduru
+ */
+public class TestClassFor_Tesseractpart1 {
+    public static void main(){
+        Tesseractpart1 test=new Tesseractpart1();
+        test.getOutput();
+        //test.getOutput("/Users/samarthmadduru/Downloads/random.png");
+    }
+}
